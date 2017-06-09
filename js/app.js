2b011657a25196c3afc958a089b44cac23215311
Luz Gutierrez <mili01gm@gmail.com>
2017-06-09 10:41:47 -0500

    terminada

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
-'use strict';
-
-$(_ => {
+$(function() {
     var screenVal = $('input:text');
 
-    $('input:button').on('click', (e) => {
-        var eachNumber = $(this).val;
+    $('input:button').on('click', function() {
+        var eachNumber = $(this).val();
         var currentScreen = screenVal.val();
         currentScreen += eachNumber;
         screenVal.val(currentScreen);
 
-        if ($(this).val == "c") {
-            screenVal.val("");
+        if ($(this).val() == "c") {
+            screenVal.val('');
         }
+    });
 
-        $("button").click(function(event) {
-            event.preventDefault();
-            screenVal.val(eval(screenVal.val()));
-        });
+    $('button').click(function(event) {
+        event.preventDefault();
+        screenVal.val(eval(screenVal.val()));
     });
 
 });
